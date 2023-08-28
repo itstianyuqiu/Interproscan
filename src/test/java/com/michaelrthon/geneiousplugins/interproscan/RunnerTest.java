2df67fee21e6802dfc1ba8307ca30b2d7ff08c8d
mattqiu <matt.qiu@veovo.com>
2023-08-29 08:57:07 +1200

    Update Unit tests

--- src/test/java/com/michaelrthon/geneiousplugins/interproscan/RunnerTest.java
+++ src/test/java/com/michaelrthon/geneiousplugins/interproscan/RunnerTest.java
@@ -4,19 +4,21 @@ import com.biomatters.geneious.publicapi.documents.AnnotatedPluginDocument;
 import com.biomatters.geneious.publicapi.documents.DocumentUtilities;
 import com.biomatters.geneious.publicapi.implementations.sequence.DefaultAminoAcidSequence;
 import com.biomatters.geneious.publicapi.plugin.DocumentOperationException;
-import com.biomatters.geneious.publicapi.plugin.SequenceAnnotationGenerator;
 import com.biomatters.geneious.publicapi.plugin.TestGeneious;
+import com.biomatters.geneious.publicapi.plugin.SequenceAnnotationGenerator;
 import jebl.util.ProgressListener;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
 public class RunnerTest {
 
-    @Test
-    public void testExampleSequence_withDefaultOptions_returnsExpectedResults() throws DocumentOperationException {
+    private AnnotatedPluginDocument apd;
+    private InterproscanOptions interproscanOptions;
+
+    @BeforeEach
+    public void setUp() {
         TestGeneious.initialize();
         DefaultAminoAcidSequence sequence = new DefaultAminoAcidSequence(
             "NARH",
@@ -28,14 +30,33 @@ public class RunnerTest {
                 "GDGCHGSDTKFNLFNSRRIDAIDVTSKTEPHP"
         );
 
-        AnnotatedPluginDocument apd = DocumentUtilities.createAnnotatedPluginDocument(sequence);
-        AnnotatedPluginDocument[] apds = {apd};
-        InterproscanOptions interproscanOptions = new InterproscanOptions();
+        apd = DocumentUtilities.createAnnotatedPluginDocument(sequence);
+        interproscanOptions = new InterproscanOptions();
         // Setting required email option
         interproscanOptions.getOption("emailAddress").setValueFromString("test@geneious.com");
-        // All other options can remain default
+    }
 
+    @Test
+    public void testExampleSequence_withDefaultOptions_returnsExpectedResults() throws DocumentOperationException {
+        AnnotatedPluginDocument[] apds = {apd};
         List<SequenceAnnotationGenerator.AnnotationGeneratorResult> results = new Runner(apds, interproscanOptions, ProgressListener.EMPTY).scanSequences();
         assertEquals(13, results.get(0).getAnnotationsToAdd().size());
+    }
+
+    @Test
+    public void testEmptySequenceInput_returnsEmptyResults() throws DocumentOperationException {
+        AnnotatedPluginDocument[] apds = new AnnotatedPluginDocument[0];
+        List<SequenceAnnotationGenerator.AnnotationGeneratorResult> results = new Runner(apds, interproscanOptions, ProgressListener.EMPTY).scanSequences();
+        assertTrue(results.isEmpty());
+    }
+
+    @Test
+    public void testInvalidEmailInput_throwsException() {
+        interproscanOptions.getOption("emailAddress").setValueFromString("invalidEmail");
+        AnnotatedPluginDocument[] apds = {apd};
+
+        assertThrows(DocumentOperationException.class, () -> {
+            new Runner(apds, interproscanOptions, ProgressListener.EMPTY).scanSequences();
+        });
     }
 }
